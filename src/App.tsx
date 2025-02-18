47c26910d1fc43356d00a6ea89a94b25b81f861e
Minsuh Roh <127650200+rohgun@users.noreply.github.com>
2025-02-18 18:29:57 +0900

    change animate

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,47 @@
 import styled from "styled-components";
-import { motion, useMotionValue, useMotionValueEvent } from "framer-motion";
+import { motion, useMotionValue, useMotionValueEvent, useTransform, useScroll, AnimatePresence } from "framer-motion";
+import { useEffect, useState } from "react";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
 height: 100vh;
+width: 100vw;
 display: flex;
-justify-content: center;
+justify-content: space-around;
 align-items: center;
 `;
-const BiggerBox = styled(motion.div)`
-background-color: rgb(117,45,255);
-display: flex;
-justify-content: center;
-
-align-items: center;
-border-radius: 40px;
-  width: 600px;
-  height: 600px;
-`;
-
 const Box = styled(motion.div)`
-width: 200px;
-height: 200px;
-display: grid;
-grid-template-columns: repeat(2, 1fr);
-background-color: white;
+width: 400px;
+height: 400px;
 border-radius: 40px;
-box-shadow: 0 2px 3px rgba(0,0,0,0.1), 0 10px 20px(0,0,0,0.06);
+background-color: white;
+box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0,0,0,0.06); 
+display: flex;
+justify-content: center;
+align-items: center;
+
+
+font-size: 28px;
+
 `;
 
+const Circle = styled(motion.div)`
+ background-color: #00A5FF;
+ height: 100px;
+ width: 100px;
+ 
+ box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0,0,0,0.06); 
+
+`;
 
 
 function App() {
-  const x = useMotionValue(0);
-  
-  
+const [clicked, setClicked] = useState(false);
+const toggleClicked = () => setClicked((prev => !prev));
 
   return (
-    <Wrapper>
-      <button onClick={() => x.set(290)}>click me</button>
-      <Box style={{ x }}  drag="x" dragSnapToOrigin />
+    <Wrapper onClick={toggleClicked}>
+     <Box>{!clicked ?<Circle layoutId="circle" style={{borderRadius:50}} />: null}</Box>        
+     <Box>{clicked? <Circle layoutId="circle" style={{borderRadius:0, scale: 2}}  /> : null}</Box>        
     </Wrapper>
   );
 }
