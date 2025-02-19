444bb313cab572dfada41520c8b179a38362c3f0
Minsuh Roh <127650200+rohgun@users.noreply.github.com>
2025-02-19 16:12:40 +0900

    animation mini end

--- src/App.tsx
+++ src/App.tsx
@@ -10,38 +10,50 @@ justify-content: space-around;
 align-items: center;
 `;
 const Box = styled(motion.div)`
-width: 400px;
-height: 400px;
+height: 200px;
 border-radius: 40px;
 background-color: white;
-box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0,0,0,0.06); 
+box-shadow:  0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0,0,0,0.06); 
+`;
+
+const Grid= styled(motion.div)`
+display: grid;
+gap: 10px;
+width: 50vw;
+grid-template-columns: repeat(3, 1fr);
+div:first-child, div:last-child{
+  grid-column:  span 2; 
+}
+`;
+
+const Overlay = styled(motion.div)`
+width: 100%;
+height: 100%;
+position: absolute; 
 display: flex;
 justify-content: center;
 align-items: center;
-
-
-font-size: 28px;
-
 `;
-
-const Circle = styled(motion.div)`
- background-color: #00A5FF;
- height: 100px;
- width: 100px;
- 
- box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0,0,0,0.06); 
-
-`;
-
 
 function App() {
-const [clicked, setClicked] = useState(false);
-const toggleClicked = () => setClicked((prev => !prev));
-
+  const [id, setId] = useState<null | string>(null);
   return (
-    <Wrapper onClick={toggleClicked}>
-     <Box>{!clicked ?<Circle layoutId="circle" style={{borderRadius:50}} />: null}</Box>        
-     <Box>{clicked? <Circle layoutId="circle" style={{borderRadius:0, scale: 2}}  /> : null}</Box>        
+    <Wrapper>
+      <Grid>
+       {["1","2","3","4"].map((n)=> (
+        <Box onClick={() => setId(n)}
+        key={n} layoutId={n} />))}
+      </Grid>
+      <AnimatePresence>
+      {id ?<Overlay 
+      onClick={() => setId(null)}
+      initial={{backgroundColor: "rgba(0,0,0,0)"}}
+       animate={{backgroundColor: "rgba(0,0,0,0.5)"}}
+        exit={{backgroundColor: "rgba(0,0,0,0)"}}
+      > 
+        <Box layoutId={id} style={{width:400, height: 200}} />
+      </Overlay> : null}
+      </AnimatePresence>
     </Wrapper>
   );
 }
