ee901bd5657592f9515d27b72a88c85bc3e130ff
Minsuh Roh <127650200+rohgun@users.noreply.github.com>
2025-02-21 19:25:33 +0900

    9-12
    2 class later

--- src/index.tsx
+++ src/index.tsx
@@ -66,9 +66,9 @@ table {
   body {
   font-weight: 300;
   font-family: 'Source Sans Pro', sans-serif;
-  color:black; 
+  color:${(props) => props.theme.white.darker}; 
   line-height: 1.2;
-  background: white;
+  background: black;
   }
   a{
     text-decoration: none; 
